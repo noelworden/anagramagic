4a74c0dbeef2652f1b03c585879348ccb1ab220a
Noel Worden <noelworden@gmail.com>
2018-02-16 14:09:56 -0700

    [FEAT] Initial setup of data and basic output

--- db/migrate/20180215233137_create_anagrams.rb
+++ db/migrate/20180215233137_create_anagrams.rb
@@ -1,7 +1,8 @@
 class CreateAnagrams < ActiveRecord::Migration[5.0]
   def change
     create_table :anagrams do |t|
-      t.string :base
+      t.string  :word
+      t.integer :count
 
       t.timestamps
     end
