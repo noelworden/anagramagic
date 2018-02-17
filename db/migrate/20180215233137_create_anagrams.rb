ff9ec0524ba096d4bc5a6428535067d3c96d38ab
Noel Worden <noelworden@gmail.com>
2018-02-17 10:54:23 -0700

    [FEAT] Basic JSON funtionality, tested

--- db/migrate/20180215233137_create_anagrams.rb
+++ db/migrate/20180215233137_create_anagrams.rb
@@ -2,7 +2,7 @@ class CreateAnagrams < ActiveRecord::Migration[5.0]
   def change
     create_table :anagrams do |t|
       t.string  :word
-      t.integer :count
+      t.integer :letter_count
 
       t.timestamps
     end
