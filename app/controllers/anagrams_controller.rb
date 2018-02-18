d60d48b9eca6e36755d907bb89b87ed897fdaef6
Noel Worden <noelworden@gmail.com>
2018-02-18 09:27:25 -0700

    [CHG] ---------------------------------------- Basic funcionality, tests passing

--- app/controllers/anagrams_controller.rb
+++ app/controllers/anagrams_controller.rb
@@ -1,23 +1,28 @@
 class AnagramsController < ApplicationController
   before_action :set_anagram, only: [:show, :destroy]
 
-  # curl http://localhost:3000/anagrams
-  def index
-    @anagrams = Anagram.all
-    json_response(@anagrams)
-    #TODO loose this action
-  end
-
   # curl http://localhost:3000/anagrams/`word`
   def show
-    json_response(@anagram)
+    render json: @anagram, limit: params[:limit]
     #TODO need to get nil search to kick back proper ActiveRecord error mesages
   end
 
-  # curl --data "word=testingcurl" http://localhost:3000/anagrams
+  # curl -X POST http://localhost:3000 -d "words=XOXO"  <- works better
   def create
-    @anagram = Anagram.create!(anagram_params)
-    json_response(@anagram, :created)
+    # TODO NOTES couldnt find a way that Rails did this automatically,
+    # kept getting errors stating a status could only be shown once per action
+    success = []
+    array = params[:words].split(", ")
+
+    array.each do |word|
+      @anagram = Anagram.create!(word: word)
+      if @anagram.save
+        success << @anagram
+      end
+    end
+
+    #will automatically break and display error, only setting up for success message
+    render json: success.to_json, status: 201
   end
 
   # curl -X "DELETE" http://localhost:3000/anagrams/tester
@@ -26,13 +31,15 @@ class AnagramsController < ApplicationController
     head :no_content
   end
 
+  # curl -X "DELETE" http://localhost:3000/anagrams
+  def destroy_all
+    Anagram.all.each(&:destroy)
+    head :no_content
+  end
+
   private
 
   def set_anagram
     @anagram = Anagram.find_by(word: params[:word])
-  end
-
-  def anagram_params
-    params.permit(:word, :letter_count)
   end
 end
