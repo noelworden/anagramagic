aac9c2bd2cf495b13319fd7177c6f227274383e6
Noel Worden <noelworden@gmail.com>
2018-02-18 09:27:25 -0700

    [CHG] Basic funcionality, tests passing

--- db/migrate/20180215233137_create_anagrams.rb
+++ db/migrate/20180215233137_create_anagrams.rb
@@ -2,7 +2,7 @@ class CreateAnagrams < ActiveRecord::Migration[5.0]
   def change
     create_table :anagrams do |t|
       t.string  :word
-      t.integer :letter_count
+      t.string :sorted_word
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,9 @@ ActiveRecord::Schema.define(version: 20180215233137) do
 
   create_table "anagrams", force: :cascade do |t|
     t.string   "word"
-    t.integer  "count"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "sorted_word"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
