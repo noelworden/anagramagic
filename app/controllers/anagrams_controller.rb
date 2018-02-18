372a068dcab8b5aebe410c51ba64757e4fbabf7d
Noel Worden <noelworden@gmail.com>
2018-02-18 11:04:13 -0700

    [FEAT] Included enpoint for details of corpus, fully tested

--- app/controllers/anagrams_controller.rb
+++ app/controllers/anagrams_controller.rb
@@ -1,13 +1,24 @@
 class AnagramsController < ApplicationController
   before_action :set_anagram, only: [:show, :destroy]
 
-  # curl http://localhost:3000/anagrams/`word`
+  # curl http://localhost:3000/anagrams/`word`?limit:__
   def show
     render json: @anagram, limit: params[:limit]
     #TODO need to get nil search to kick back proper ActiveRecord error mesages
   end
 
-  # curl -X POST http://localhost:3000 -d "words=XOXO"  <- works better
+  def corpus_detail
+    word_lengths = Anagram.all.pluck(:word_length)
+
+    render json: { "Total Corpus Count": "#{Anagram.all.count}",
+                   "Minimum Word Length": "#{word_lengths.min}",
+                   "Maximum Word Length": "#{word_lengths.max}",
+                   "Median Word Length": "#{median(word_lengths)}",
+                   "Average Word Length": "#{(word_lengths.sum.to_f / word_lengths.count.to_f).round(3)}"
+                 }, status: 200
+  end
+
+  # curl -X POST http://localhost:3000 -d "words=XOXO"
   def create
     # TODO NOTES couldnt find a way that Rails did this automatically,
     # kept getting errors stating a status could only be shown once per action
@@ -41,5 +52,11 @@ class AnagramsController < ApplicationController
 
   def set_anagram
     @anagram = Anagram.find_by(word: params[:word])
+  end
+
+  def median(array)
+    sorted = array.sort
+    length = sorted.length
+    (sorted[(length - 1) / 2] + sorted[length / 2]) / 2.0
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
 #TODO change this for actual dictionary
 #TODO should it be downcasing everything? If so they should be unique
+#TODO NOTES takes super long, not sure how to optimize
 file = 'data/mini_dictionary.txt'
 File.readlines(file).each do |line|
-  Anagram.create(word: line.downcase.strip)
+  word = line.strip
+  Anagram.create(word: word)
 
   p word
 end
