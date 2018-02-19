37abcf50b234fdf79f53ed56ae6b6e2d56e03fba
Noel Worden <noelworden@gmail.com>
2018-02-19 14:46:44 -0700

    [CHG] ---------------------------------------- Updated `controller` comments & specs for new versioning routes

--- spec/requests/anagrams_spec.rb
+++ spec/requests/anagrams_spec.rb
@@ -11,8 +11,8 @@ RSpec.describe 'Anagrams API', type: :request do
                     }
   let(:word) { Anagram.first.word }
 
-  describe 'GET /anagrams/:word' do
-    before { get "/anagrams/#{word}" }
+  describe 'GET /api/v1/anagrams/:word' do
+    before { get "/api/v1/anagrams/#{word}" }
 
     it 'returns all anagrams' do
       expect(json).not_to be_empty
@@ -24,8 +24,8 @@ RSpec.describe 'Anagrams API', type: :request do
     end
   end
 
-  describe 'GET /anagrams/:word?limit=3' do
-    before { get "/anagrams/#{word}/?limit=3" }
+  describe 'GET /api/v1/anagrams/:word?limit=3' do
+    before { get "/api/v1/anagrams/#{word}/?limit=3" }
 
     it 'returns only non-proper nouns' do
       expect(json["anagrams"]).to eq(["Dear", "Read", "dare"])
@@ -36,8 +36,8 @@ RSpec.describe 'Anagrams API', type: :request do
     end
   end
 
-  describe 'GET /anagrams/:word?proper_nouns=false' do
-    before { get "/anagrams/#{word}/?proper_nouns=false" }
+  describe 'GET /api/v1/anagrams/:word?proper_nouns=false' do
+    before { get "/api/v1/anagrams/#{word}/?proper_nouns=false" }
 
     it 'returns only non-proper nouns' do
       expect(json["anagrams"]).to eq(["dare", "read"])
@@ -48,35 +48,35 @@ RSpec.describe 'Anagrams API', type: :request do
     end
   end
 
-  describe 'GET /anagram-compare' do
+  describe 'GET /api/v1/anagram-compare' do
     it "should show error if only one entry" do
-      get "/anagram-compare", params: { words: "yyyy"}
+      get "/api/v1/anagram-compare", params: { words: "yyyy"}
       expect(response.body).to eq("Please check your word count, you need exactly two words")
       expect(response).to have_http_status(404)
     end
 
     it "should show error if more than two entries" do
-      get "/anagram-compare", params: { words: "xxxx, yyyy, zzzz"}
+      get "/api/v1/anagram-compare", params: { words: "xxxx, yyyy, zzzz"}
       expect(response.body).to eq("Please check your word count, you need exactly two words")
       expect(response).to have_http_status(404)
     end
 
     it "should show 'false' if pair of words are not anagrams" do
-      get "/anagram-compare", params: { words: "dear, yyyy"}
+      get "/api/v1/anagram-compare", params: { words: "dear, yyyy"}
       expect(response.body).to eq("false")
       expect(response).to have_http_status(200)
     end
 
     it "should show 'true' if pair of words are anagrams" do
-      get "/anagram-compare", params: { words: "dear, read"}
+      get "/api/v1/anagram-compare", params: { words: "dear, read"}
       expect(response.body).to eq("true")
       expect(response).to have_http_status(200)
     end
   end
 
 
-  describe 'GET /anagrams/corpus-detail' do
-    before { get "/corpus-detail" }
+  describe 'GET /api/v1/anagrams/corpus-detail' do
+    before { get "/api/v1/corpus-detail" }
 
     it 'should not be an empty return' do
       expect(json).not_to be_empty
@@ -95,71 +95,70 @@ RSpec.describe 'Anagrams API', type: :request do
     end
   end
 
-  describe 'GET /anagrams-list/:integer' do
+  describe 'GET /api/v1/anagrams-list/:integer' do
     it 'should return results if correct integer is used' do
-      get "/anagrams-list/2"
+      get "/api/v1/anagrams-list/2"
       expect(json[0]).to eq(["Dear", "Read", "dare", "dear", "read"])
     end
 
     it 'should return 404 if incorrect integer is used' do
-      get "/anagrams-list/8"
+      get "/api/v1/anagrams-list/8"
       expect(response).to have_http_status(404)
     end
   end
 
   describe 'GET /big-ol-anagram' do
     it 'should list of largest anagram' do
-      get "/big-ol-anagram"
+      get "/api/v1/big-ol-anagram"
       expect(json).to eq(["Dear", "Read", "dare", "dear", "read"])
     end
   end
 
-  describe 'POST /' do
+  describe 'POST /api/v1/anagrams' do
     let (:valid_attributes) { { words: "test01, test02" } }
 
     # TODO NOTES had difficulty using 'before' when trying to get a database count
     it 'increases Anagram count by 2' do
       expect {
-        post '/', params: valid_attributes
+        post '/api/v1/anagrams', params: valid_attributes
       }.to change(Anagram, :count).by(2)
     end
 
     it 'returns status code 201' do
-      post '/', params: valid_attributes
+      post '/api/v1/anagrams', params: valid_attributes
       expect(response).to have_http_status(201)
     end
   end
 
-  describe 'DELETE /anagrams/:word' do
-    # before { delete "/anagrams/#{word}" }
+  describe 'DELETE /api/v1/anagrams/:word' do
     it 'deletes a single record' do
-      expect { delete "/anagrams/#{word}" }.to change(Anagram, :count).by(-1)
+      expect { delete "/api/v1/anagrams/#{word}" }.to change(Anagram, :count).by(-1)
     end
 
     it 'returns status code 204' do
-      delete "/anagrams/#{word}"
+      delete "/api/v1/anagrams/#{word}"
       expect(response).to have_http_status(204)
     end
   end
 
-  describe 'DELETE_ALL /anagrams/' do
+  describe 'DELETE_ALL /api/v1/anagrams/' do
     it 'deletes all records' do
-      expect { delete '/anagrams' }.to change(Anagram, :count).to(0)
+      expect { delete '/api/v1/anagrams' }.to change(Anagram, :count).to(0)
     end
 
     it 'returns status code 204' do
-      delete "/anagrams"
+      delete "/api/v1/anagrams"
       expect(response).to have_http_status(204)
     end
   end
 
-  describe 'DELETE_ONE /anagrams/:word/destroy_anagram' do
+  describe 'DELETE_ONE /api/v1/anagrams/:word/destroy_anagram' do
     it 'deletes all records' do
-      expect { delete "/anagrams/#{word}/destroy_anagram" }.to change(Anagram, :count).to(1)
+      expect { delete "/api/v1/anagrams/#{word}/destroy_anagram" }.to change(Anagram, :count).to(1)
     end
 
     it 'returns status code 204' do
-      delete "/anagrams"
+      delete "/api/v1/anagrams"
       expect(response).to have_http_status(204)
     end
   end
