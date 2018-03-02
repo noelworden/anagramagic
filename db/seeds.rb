b342923f852b6d2034b8d893202c7df06f966309
Noel Worden <noelworden@gmail.com>
2018-03-02 14:41:18 -0700

    [REFAC] Refactored `seed.rb` on be more efficient

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-file = 'data/dictionary.txt'
-File.readlines(file).each do |line|
-  word = line.strip
-  Anagram.create(word: word)
+p "Seeding Commenced at #{Time.zone.now.in_time_zone('Eastern Time (US & Canada)').strftime("%l:%M:%S %p %Z")}"
 
-  p word
+file = 'data/dictionary.txt'
+corpus = []
+
+File.readlines(file).each do |line|
+  corpus << Anagram.new(word: line.strip)
 end
+
+Anagram.import corpus
+
+p "Seeding Concluded at #{Time.zone.now.in_time_zone('Eastern Time (US & Canada)').strftime("%l:%M:%S %p %Z")}"
